9259f02cbe02ffcb617010c982ec80d06c3f8c92
huangjianbo <huangjianbo@bupt.edu.cn>
2018-04-06 16:36:35 +0800

    fix a bug

--- MultiFilesOutput/code/output_with_sorting(bug).cpp
+++ MultiFilesOutput/code/output_with_sorting(bug).cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define SIZE 30
 
 //文件内容排序
-class OrderlyFiles {
+        class OrderlyFiles {
     public:
         int num;
         string key;
@@ -29,15 +29,14 @@ class OrderlyFiles {
             file_value_name = "ps_00" + to_string(i) + "_value.bin";
             ifstream fin_key(file_key_name, ios::in);
             ifstream fin_value(file_value_name, ios::in);
-            fin_key.seekg(0, ios::beg);
-            m = fin_key.tellg();
             fin_key.seekg(0, ios::end);
             n = fin_key.tellg();
-            char *kbuff = new char[n-m];
+            char *kbuff = new char[n];
             fin_key.seekg(0, ios::beg);
-            fin_key.read((char*)kbuff,n-m);
-            for(j = 0; j <= n-m; j++) {
-                if(kbuff[j] != ' ') key.push_back(kbuff[j]);
+            fin_key.read((char*)kbuff,n);
+            j = 0;
+            while(j < n) {
+                if(kbuff[j] != ' ') key.push_back(kbuff[j++]);
                 else {
                     offset = *((int*)&kbuff[++j]);
                     j += 4;
@@ -46,11 +45,13 @@ class OrderlyFiles {
                     char *vbuff = new char[value_length];
                     fin_value.read(vbuff,value_length);
                     for(k = 0; k < value_length; k++) value.push_back(vbuff[k]);
-                    if(key.size() != 0 && value.size() != 0) mymap.insert(pair<string,string>(key,value));
+                    if(key.size() != 0 && value.size() != 0) { 
+                        mymap.insert(pair<string,string>(key,value));
+                    }
                     key.clear();
                     value.clear();
                     delete[] vbuff;
-                    j+=3;
+                    j+=4;
                 }
             }
             fin_key.close();
@@ -65,14 +66,10 @@ class OrderlyFiles {
             int value_length;
             file_key_name = "ps_00" + to_string(x) + "_key.bin";
             file_value_name = "ps_00" + to_string(x) + "_value.bin";
-            ofstream fout_key_resort(file_key_name, ios::trunc);
-            ofstream fout_value_resort(file_value_name, ios::trunc);
-            fout_key_resort.close();
-            fout_value_resort.close();
+            ofstream fout_key_resort(file_key_name, ios::out);
+            ofstream fout_value_resort(file_value_name, ios::out);
             map<string,string>::iterator it = mymap.begin();
             for(; it != mymap.end(); it++) {
-                ofstream fout_key_resort(file_key_name, ios::app);
-                ofstream fout_value_resort(file_value_name,ios::app);
                 fout_key_resort << it->first << " ";
                 value_length = it->second.size();
                 fout_key_resort.write((char*)&offset,sizeof(int));
@@ -85,6 +82,8 @@ class OrderlyFiles {
                     break;
                 }
             }
+            fout_key_resort.close();
+            fout_value_resort.close();
             mymap.erase(mymap.begin(), it);
         }
         
@@ -123,6 +122,8 @@ class MultifulFilesWriting {
 
         void WriteToFiles() {
             ifstream fin(fin_file_name, ios::in);
+            ofstream fout_key(key_file_name, ios::out);
+            ofstream fout_value(value_file_name, ios::out);
             while(getline(fin, linestr)) {
                 if(!linestr.size()) continue;
                 count++;
@@ -131,10 +132,6 @@ class MultifulFilesWriting {
                 value = linestr.substr(a+1);
                 file_num = count/SIZE + 1;
                 value_length = value.size();
-                
-                ofstream fout_key(key_file_name, ios::app);
-                ofstream fout_value(value_file_name, ios::app);
-
                 //输出到key文件中
                 fout_key << key << " ";
                 fout_key.write((char*)&offset, sizeof(int));
@@ -144,25 +141,37 @@ class MultifulFilesWriting {
                 offset += (value_length + sizeof(char));
                 key.clear();
                 value.clear();
-                if(count%(SIZE+1) == 0 && count/(SIZE+1) > 0) {
-                    OrderlyFiles* rec = new OrderlyFiles((count-1)/SIZE);
+                if(count%SIZE == 0 && count/SIZE > 0) {
+                    fout_key.close();
+                    fout_value.close();
+                    OrderlyFiles* rec = new OrderlyFiles(count/SIZE);
                     rec->SortFiles();
                     delete rec;
+                    key_file_name = "ps_00" + to_string(file_num) + "_key.bin";
+                    value_file_name = "ps_00" + to_string(file_num) + "_value.bin";
+                    fout_key.open(key_file_name, ios::out);
+                    fout_value.open(value_file_name, ios::out);
                 }
                 //判断是否超过SIZE,超过则变成对应的文件名
-                if(file_num - file_flag != 1 ) {
+                /*if(file_num - file_flag != 1 ) {
+                    fout_key.close();
+                    fout_value.close();
                     file_flag++;
                     key_file_name = "ps_00" + to_string(file_num) + "_key.bin";
                     value_file_name = "ps_00" + to_string(file_num) + "_value.bin";
+                    fout_key.open(key_file_name, ios::out);
+                    fout_value.open(value_file_name, ios::out);
                     offset = 0;
-                }   
+                }*/   
+            }
+            fout_key.close();
+            fout_value.close();
+            //将最后一个文件排序
+            /*OrderlyFiles* rec = new OrderlyFiles(count/SIZE+1);
+            rec->SortFiles();
+            delete rec;*/
         }
-        //将最后一个文件排序
-        OrderlyFiles* rec = new OrderlyFiles((count-1)/SIZE+1);
-        rec->SortFiles();
-        delete rec;
-    }
-};
+    };
 
 string MultifulFilesWriting::key_file_name = "ps_001_key.bin";
 string MultifulFilesWriting::value_file_name = "ps_001_value.bin";
