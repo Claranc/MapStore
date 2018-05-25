def8d34e356afd6452c97036a27f48021a57ad0b
Claranc <fxj_bupt@163.com>
2018-05-26 00:11:15 +0800

    v6.0

---
+++ ring1/ring.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <vector>
+#include <thread>
+using namespace std;
+
+//不注释这一行代表环形发送，注释这一行代表ALL2ALL
+#define RING
+
+//定义节点信息结构体
+struct server_info {
+    string ip_addr;
+    int udp_port;
+};
